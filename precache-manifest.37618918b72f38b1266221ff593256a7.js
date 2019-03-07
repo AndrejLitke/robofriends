a0700c41f9d87d00937106524f0940e1ba192cbb
Andrej Litke <andrej.litke@aumann.com>
2019-03-07 12:06:26 +0100

    Updates

---
+++ precache-manifest.37618918b72f38b1266221ff593256a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "e09c17eb09c6184ad642",
+    "url": "/robofriends/static/js/main.e09c17eb.chunk.js"
+  },
+  {
+    "revision": "860b8f9e03811d2cbd9a",
+    "url": "/robofriends/static/js/2.860b8f9e.chunk.js"
+  },
+  {
+    "revision": "e09c17eb09c6184ad642",
+    "url": "/robofriends/static/css/main.feca69f5.chunk.css"
+  },
+  {
+    "revision": "860b8f9e03811d2cbd9a",
+    "url": "/robofriends/static/css/2.5054de12.chunk.css"
+  },
+  {
+    "revision": "c58e221f27926e9925124e5d64aa08c0",
+    "url": "/robofriends/index.html"
+  }
+];
